151a25e0256fb698b5838954b6435afba786b62b
Nick-ch <nd.chernikov@gmail.com>
2017-11-25 22:41:39 +0300

    fix bug with page name

--- routes/verified.js
+++ routes/verified.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET verify page */
+/* GET verified page */
 router.get('/', function (req, res, next) {
-  res.render('verify');
+  res.render('verified');
 });
 
 module.exports = router;
