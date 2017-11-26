8c3b957bd6aa689ee795d5ab72a5a211a167f42d
Nick-ch <nd.chernikov@gmail.com>
2017-11-26 18:10:04 +0300

    handle first stage of auth

--- routes/verified.js
+++ routes/verified.js
@@ -2,10 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET verified page */
+/* GET verification page */
 router.get('/', (req, res) => {
-  const param = req.params[0];
-  res.render('verified', { status: param });
+  if (req.query.error) {
+    res.render('verified', { status: 'Ошибка!', message: 'Доступ к вашему аккаунту не был предоставлен.' });
+  } else if (req.query.code) {
+    res.render('verified', { status: 'Авторизация успешна!' });
+  } else {
+    res.render('verified', { status: 'Авторизация не пройдена', message: 'Попробуйте ещё раз' });
+  }
 });
 
 module.exports = router;
