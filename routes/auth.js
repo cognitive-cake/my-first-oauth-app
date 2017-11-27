d426e11056fa04a4dba97a39fa8e1828b66ee737
cognitive-cake <nd.chernikov@gmail.com>
2017-11-27 16:21:12 +0300

    try understand how work response stream

--- routes/auth.js
+++ routes/auth.js
@@ -6,16 +6,16 @@ const parameters = {
   client_id: 6273123,
   client_secret: 'k8BFc9oForp6nfjkfi1U',
   redirect_uri: 'https://arcane-cliffs-64611.herokuapp.com/auth',
+  homepage: 'https://arcane-cliffs-64611.herokuapp.com',
 };
 
 
 /* GET Access token */
 const getToken = (code) => {
-  return request.get(`https://oauth.vk.com/access_token?client_id=${parameters.client_id}&client_secret=${parameters.client_secret}&code=${code}&redirect_uri=${parameters.redirect_uri}`, (err, res, resBody) => {
+  request.get(`https://oauth.vk.com/access_token?client_id=${parameters.client_id}&client_secret=${parameters.client_secret}&code=${code}&redirect_uri=${parameters.redirect_uri}`, (err, res, resBody) => {
     console.error('error:', err);
     console.log('statusCode:', res && res.statusCode);
     console.log(resBody);
-    return resBody;
   });
 };
 
@@ -25,25 +25,28 @@ router.get('/', (req, res) => {
     console.error(`Auth error: ${req.query.error}`);
     console.error(`Error description: ${req.query.error_description}`);
     res.render('auth', { status: 'Ошибка!', message: 'Доступ к вашему аккаунту не был предоставлен.' });
+    console.log('Im print after res.render!')
   } else if (req.query.code) {
     res.render('auth', { status: 'Авторизация успешна!' });
-    const token = getToken(req.query.code);
-    res.cookie('access_token', token.access_token, {
-      domain: '.herokuapp.com',
-      expires: new Date(Date.now() + token.expires_in),
-      httpOnly: true,
-      secure: true,
-    });
-    res.cookie('user_id', token.user_id, {
-      domain: '.herokuapp.com',
-      expires: new Date(Date.now() + token.expires_in),
-      httpOnly: true,
-      secure: true,
-    });
-    console.log(token);
+    getToken(req.query.code);
   } else {
     res.render('auth', { status: 'Авторизация не пройдена', message: 'Попробуйте ещё раз' });
+    res.redirect(parameters.homepage);
   }
 });
 
 module.exports = router;
+
+/* console.log(token);
+res.cookie('access_token', token.access_token, {
+  domain: '.herokuapp.com',
+  expires: new Date(Date.now() + token.expires_in),
+  httpOnly: true,
+  secure: true,
+});
+res.cookie('user_id', token.user_id, {
+  domain: '.herokuapp.com',
+  expires: new Date(Date.now() + token.expires_in),
+  httpOnly: true,
+  secure: true,
+}); */
