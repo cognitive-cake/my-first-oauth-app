994f509a96da0dd6c89b0ea94f110783c01fe841
Nick-ch <nd.chernikov@gmail.com>
2017-12-03 15:50:49 +0300

    Фуууууууф.... Так и не нашёл способа обработать результат асинхронного запроса к серверу VK. Нужно больше фундаментальных знаний по Node.js

--- routes/auth.js
+++ routes/auth.js
@@ -10,28 +10,11 @@ const parameters = {
   homepage: 'https://arcane-cliffs-64611.herokuapp.com',
 };
 
-let token = '';
-console.log('raw token:', token);
-
 /* GET Access token */
-const getToken = (code, cb) => {
-  request.get(
-    `https://oauth.vk.com/access_token?client_id=${parameters.client_id}&client_secret=${parameters.client_secret}&code=${code}&redirect_uri=${parameters.redirect_uri}`,
-    (error, response, body) => {
-      if (error) {
-        console.log(error);
-      }
-      console.log('statusCode:', response && response.statusCode);
-      console.log('body:', body);
-      token = body;
-      console.log('stored token:', token);
-      cb();
-    },
-  );
-};
-
-const redirect = () => {
-
+const getToken = (code) => {
+  request
+    .get(`https://oauth.vk.com/access_token?client_id=${parameters.client_id}&client_secret=${parameters.client_secret}&code=${code}&redirect_uri=${parameters.redirect_uri}`)
+    .on('error', err => console.log(err));
 };
 
 /* GET authorization page */
@@ -39,20 +22,22 @@ router.get('/', (req, res) => {
   console.log('Cookies!:', req.cookies);
   if (req.cookies.access_token) {
     console.log('token:', req.cookies.access_token);
-    res.redirect('auth/success');
+    res.redirect('auth/session');
   } else if (req.query.error) {
     console.error(`Auth error: ${req.query.error}`);
     console.error(`Error description: ${req.query.error_description}`);
     res.render('auth', { status: 'Ошибка!', message: 'Доступ к вашему аккаунту не был предоставлен.' });
   } else if (req.query.code) {
-    getToken(req.query.code, redirect);
-    res.render('auth', { status: 'Идёт авторизация...' });
+    res
+      .cookie('test', 'cookie')
+      .render('auth', { status: 'Идёт авторизация...' });
+    getToken(req.query.code);
   } else {
     res.render('auth', { status: 'Авторизация не пройдена', message: 'Попробуйте ещё раз' });
   }
 });
 
-router.get('/success', (req, res) => {
+router.get('/session', (req, res) => {
   res.render('auth', { status: 'Токен найден!' });
 });
 
